473b9bc751777d5b68588fb7b97b760c600ff083
Adar Hefer <adar.hefer@gmail.com>
2022-01-24 18:12:09 +0200

    Initial Commit

---
+++ NowPlayingRetriever/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NowPlayingRetriever
+//
+//  Created by Adar Hefer on 24/01/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
