a7898c921d3c3251b9a6a9ce8fa045c1dc0352ad
Adar Hefer <adar.hefer@gmail.com>
2022-01-24 21:07:00 +0200

    This kinda works now

--- NowPlayingRetriever/AppDelegate.swift
+++ NowPlayingRetriever/AppDelegate.swift
@@ -10,11 +10,124 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    typealias MRMediaRemoteGetNowPlayingInfoFunction = @convention(c) (DispatchQueue, @escaping ([String: Any]) -> Void) -> Void
+    typealias MRMediaRemoteRegisterForNowPlayingNotificationsFunction = @convention(c) (DispatchQueue) -> Void
+    typealias MRMediaRemoteGetNowPlayingApplicationIsPlayingFunction = @convention(c) (DispatchQueue, @escaping (Bool) -> Void) -> Void
     
-
-
+    private var getNowPlaying: MRMediaRemoteGetNowPlayingInfoFunction!
+    private var registerForNotifications: MRMediaRemoteRegisterForNowPlayingNotificationsFunction!
+    private var getIsPlaying: MRMediaRemoteGetNowPlayingApplicationIsPlayingFunction!
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        // Load framework
+        let bundle = CFBundleCreate(kCFAllocatorDefault, NSURL(fileURLWithPath: "/System/Library/PrivateFrameworks/MediaRemote.framework"))
+
+        // Get a Swift function for MRMediaRemoteGetNowPlayingInfo
+        guard let MRMediaRemoteGetNowPlayingInfoPointer = CFBundleGetFunctionPointerForName(bundle, "MRMediaRemoteGetNowPlayingInfo" as CFString) else { return }
+        getNowPlaying = unsafeBitCast(MRMediaRemoteGetNowPlayingInfoPointer, to: MRMediaRemoteGetNowPlayingInfoFunction.self)
+        
+        
+        guard let MRMediaRemoteRegisterForNowPlayingNotificationsPointer = CFBundleGetFunctionPointerForName(bundle, "MRMediaRemoteRegisterForNowPlayingNotifications" as CFString) else { return }
+        
+        registerForNotifications = unsafeBitCast(MRMediaRemoteRegisterForNowPlayingNotificationsPointer, to: MRMediaRemoteRegisterForNowPlayingNotificationsFunction.self)
+        
+        registerForNotifications(.main)
+        
+        guard let MRMediaRemoteGetNowPlayingApplicationIsPlayingPointer = CFBundleGetFunctionPointerForName(bundle, "MRMediaRemoteGetNowPlayingApplicationIsPlaying" as CFString) else { return }
+        
+        getIsPlaying = unsafeBitCast(MRMediaRemoteGetNowPlayingApplicationIsPlayingPointer, to: MRMediaRemoteGetNowPlayingApplicationIsPlayingFunction.self)
+        
+        
+        NotificationCenter.default.addObserver(forName: .init(rawValue: "kMRMediaRemoteNowPlayingApplicationDidChangeNotification"),
+                                               object: nil,
+                                               queue: .main,
+                                               using: { notification in
+            self.delayAndUpdateNowPlayingInformation()
+        })
+        
+        NotificationCenter.default.addObserver(forName: .init(rawValue: "kMRMediaRemoteNowPlayingInfoDidChangeNotification"),
+                                               object: nil,
+                                               queue: .main,
+                                               using: { notification in
+            self.delayAndUpdateNowPlayingInformation()
+        })
+        
+        NotificationCenter.default.addObserver(forName: .init(rawValue: "kMRMediaRemoteNowPlayingApplicationIsPlayingDidChangeNotification"),
+                                               object: nil,
+                                               queue: .main,
+                                               using: { notification in
+            self.delayAndUpdateNowPlayingInformation()
+        })
+        
+        updateNowPlayingInformation()
+    }
+    
+    private var currentTrack = ""
+    
+    private var timer: Timer?
+    
+    private func delayAndUpdateNowPlayingInformation() {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { _ in
+            self.updateNowPlayingInformation()
+        }
+    }
+    
+    private func updateNowPlayingInformation() {
+        getNowPlaying(.main) { [self] information in
+            getIsPlaying(.main) { [self] isPlaying in
+                guard isPlaying else {
+                    currentTrack = ""
+                    try! write(data: Data(), fileName: "artist", fileExtension: "txt")
+                    try! write(data: Data(), fileName: "track", fileExtension: "txt")
+                    try! write(data: Data(), fileName: "album", fileExtension: "txt")
+                    try! write(data: Data(), fileName: "artwork", fileExtension: "jpg")
+                    return
+                }
+                if let title = information["kMRMediaRemoteNowPlayingInfoTitle"] as? String {
+                    if currentTrack == title {
+                        return
+                    }
+                    currentTrack = title
+                    if let data = title.data(using: .utf8) {
+                        try? write(data: data, fileName: "track", fileExtension: "txt")
+                    }
+                }
+                if let artist = information["kMRMediaRemoteNowPlayingInfoArtist"] as? String,
+                    let data = artist.data(using: .utf8) {
+                    try? write(data: data, fileName: "artist", fileExtension: "txt")
+                }
+                if let album = information["kMRMediaRemoteNowPlayingInfoAlbum"] as? String,
+                   let data = album.data(using: .utf8) {
+                    try? write(data: data, fileName: "album", fileExtension: "txt")
+                }
+                if let artwork = information["kMRMediaRemoteNowPlayingInfoArtworkData"] as? Data {
+                    switch information["kMRMediaRemoteNowPlayingInfoArtworkMIMEType"] as? String {
+                    case "image/jpeg":
+                        try! write(data: artwork, fileName: "artwork", fileExtension: "jpg")
+                    default:
+                        guard let representation = NSImage(data: artwork)?.representations.first as? NSBitmapImageRep,
+                              let jpegData = representation.representation(using: .jpeg, properties: [:])
+                        else { break }
+                        
+                        try! write(data: jpegData, fileName: "artwork", fileExtension: "jpg")
+                    }
+                } else {
+                    try! write(data: Data(), fileName: "artwork", fileExtension: "jpg")
+                }
+            }
+        }
+    }
+    
+    private func write(data: Data, fileName: String, fileExtension: String) throws {
+        let fileUrl = FileManager.default
+            .homeDirectoryForCurrentUser
+            .appendingPathComponent(fileName)
+            .appendingPathExtension(fileExtension)
+        
+        try data.write(to: fileUrl)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -24,7 +137,5 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
-
-
 }
 
---
+++ setup_file_links.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ln -s ~/Library/Containers/co.AdarHefer.NowPlayingRetriever/Data/artist.txt Snip_Artist.txt
+ln -s ~/Library/Containers/co.AdarHefer.NowPlayingRetriever/Data/track.txt Snip_Track.txt
+ln -s ~/Library/Containers/co.AdarHefer.NowPlayingRetriever/Data/album.txt Snip_Album.txt
+ln -s ~/Library/Containers/co.AdarHefer.NowPlayingRetriever/Data/artwork.jpg Snip_Artwork.jpg
