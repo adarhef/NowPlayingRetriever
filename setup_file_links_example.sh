742ac6e7c24864498428fe0cdf97adbcacedcc44
Adar Hefer <adar.hefer@gmail.com>
2022-01-26 10:31:24 +0200

    Updated the example script

--- setup_file_links_example.sh
+++ setup_file_links_example.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+rm -f Snip_Artist.txt
+rm -f Snip_Track.txt
+rm -f Snip_Album.txt
+rm -f Snip_Artwork.jpg
+
 ln -s ~/Library/Containers/co.AdarHefer.NowPlayingRetriever/Data/artist.txt Snip_Artist.txt
 ln -s ~/Library/Containers/co.AdarHefer.NowPlayingRetriever/Data/track.txt Snip_Track.txt
 ln -s ~/Library/Containers/co.AdarHefer.NowPlayingRetriever/Data/album.txt Snip_Album.txt
